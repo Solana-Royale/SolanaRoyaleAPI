3d297d814a21f1d8256c94f84c61c72de8ed0f4c
VihangaTheTurtle-School <98092191+VihangaTheTurtle-School@users.noreply.github.com>
2022-04-04 01:58:11 +0100

    pp

--- src/routes/checkToken.js
+++ src/routes/checkToken.js
@@ -7,6 +7,8 @@ router.get("/", (req, res) => {
   var token = Buffer.from(req.query.token, "base64");
   var address = req.query.address;
 
+  console.log(token);
+
   var tkd = VSSI.parseToken(token, {
     userIpAddress: req.headers["x-forwarded-for"] || req.socket.remoteAddress
   });

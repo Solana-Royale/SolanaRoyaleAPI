93fecee2d1727ca25b189ed9a097bffa9d84744c
vihangatheturtle <43118457+vihangatheturtle@users.noreply.github.com>
2022-05-07 15:51:15 +0100

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,6 @@ import play from "./play.js";
 import authorize from "./authorize.js";
 import register from "./register.js";
 import checkToken from "./checkToken.js";
-import pay from "./pay.js";
 import sync from "./sync.js";
 import ping from "./ping.js";
 
@@ -16,7 +15,6 @@ router.use("/games/play", play);
 router.use("/authorize", authorize);
 router.use("/register", register);
 router.use("/checkToken", checkToken);
-router.use("/pay", pay);
 router.use("/8CjF4IygzwEyG82QPsrK/sync", sync);
 
 export default router;

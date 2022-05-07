fbd4cc19038c49fbe010a18fc12e145037cee8ef
vihangatheturtle <43118457+vihangatheturtle@users.noreply.github.com>
2022-05-07 16:20:55 +0100

    Revert "Update index.js"
    This reverts commit 93fecee2d1727ca25b189ed9a097bffa9d84744c.

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ import play from "./play.js";
 import authorize from "./authorize.js";
 import register from "./register.js";
 import checkToken from "./checkToken.js";
+import pay from "./pay.js";
 import sync from "./sync.js";
 import ping from "./ping.js";
 
@@ -15,6 +16,7 @@ router.use("/games/play", play);
 router.use("/authorize", authorize);
 router.use("/register", register);
 router.use("/checkToken", checkToken);
+router.use("/pay", pay);
 router.use("/8CjF4IygzwEyG82QPsrK/sync", sync);
 
 export default router;
